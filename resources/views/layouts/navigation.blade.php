09d8397d3279f16b57cd1b7af2004f3c80b964ec
Fudzuki <kiri11280201@gmail.com>
2022-12-01 15:40:19 +0900

    navタグを追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,28 +1,30 @@
 <!-- header -->
 <header>
-    <ul class="head1">
-        <li>
-            <img src="#" alt="logo">
-        </li>
-        <li>
-            <input type="text" value="キーワード検索" id="serch">
-            <button type="button" id="serch_b">検索</button>
-        </li>
-        <li>
-            <img src="#" alt="cart">
-        </li>
-        <li>
-            <img src="#" alt="alart">
-        </li>
-        <li>
-            <img src="#" alt="auction">
-        </li>
-        <li>
-            <img src="#" alt="like">
-        </li>
-        <li>
-            <img src="#" alt="mypage">
-        </li>
-    </ul>
-    <p class="login">ログイン/新規登録</p>
+    <nav>
+        <ul class="head1">
+            <li>
+                <img src="#" alt="logo">
+            </li>
+            <li>
+                <input type="text" placeholder="キーワード検索" id="serch">
+                <button type="button" id="serch_b">検索</button>
+            </li>
+            <li>
+                <img src="#" alt="cart">
+            </li>
+            <li>
+                <img src="#" alt="alart">
+            </li>
+            <li>
+                <img src="#" alt="auction">
+            </li>
+            <li>
+                <img src="#" alt="like">
+            </li>
+            <li>
+                <img src="#" alt="mypage">
+            </li>
+        </ul>
+        <p class="login">ログイン/新規登録</p>
+    </nav>
 </header>

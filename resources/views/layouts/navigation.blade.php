89b7425498437dea0cce903523f9291f37540404
ueda2001 <sara_melody_0927@yahoo.co.jp>
2022-12-07 19:13:21 +0900

    header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,30 +1,59 @@
-<!-- header -->
-<header>
-    <nav>
-        <ul class="head1">
-            <li>
-                <img src="{{ asset('img/logo.png') }}" alt="logo">
-            </li>
-            <li>
-                <input type="text" placeholder="キーワード検索" id="serch">
-                <button type="button" id="serch_b"></button>
-            </li>
-            <li>
-                <img src="#" alt="cart">
-            </li>
-            <li>
-                <img src="#" alt="alart">
-            </li>
-            <li>
-                <img src="#" alt="auction">
-            </li>
-            <li>
-                <img src="#" alt="like">
-            </li>
-            <li>
-                <img src="#" alt="mypage">
-            </li>
-        </ul>
-        <p class="nav_login">ログイン/新規登録</p>
-    </nav>
-</header>
+<div class="wrapper width: 100%;">
+    <!--header-->
+    <header class="header width: 100%;
+    display: flex;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.05);">
+        <!--left-->
+        <div class="logo width: 40%;
+    display: flex;
+    margin: auto;
+    padding: auto;">
+            <a href="index.php">
+                <img class=" width: 130px;
+    margin: 0 10px;" src="img/logo.png" alt="ALTE">
+            </a>
+
+
+            <!--search-->
+            <div class="search">
+                <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+                <form method="get" action="#" class="search_container">
+                    <input type="text" size="30" placeholder="キーワード検索">
+                    <input type="submit" value="&#xf002">
+                </form>
+            </div>
+        </div><!--/logo-->
+        <!--/search-->
+        <!--/left-->
+
+        <!--nav-->
+        <nav class="navigation">
+            <ul>
+                <li><a href=""><img src="img/cart.png" alt="cart"></a></li>
+                <li><a href=""><img src="img/auction.png" alt="info"></a></li>
+                <li><a href=""><img src="img/heart.png" alt="auction"></a></li>
+                <li><a href=""><img src="img/bell.png" alt="iine"></a></li>
+                <li><a href=""><img src="img/human.png" alt="mypage"></a></li>
+            </ul>
+        </nav>
+        <!--/nav-->
+    </header>
+    <!--header-->
+    <!--header2-->
+    <header class="header2">
+        <!--left-->
+        <div class="category-left">
+
+        </div>
+        <!--/left-->
+        <!--right-->
+        <div class="category-right">
+
+        </div>
+        <!--/right-->
+    </header>
+    <!--/header2-->
+
+
+</div><!--/wrapper-->

6c332491497ba0197b090516b7c78873b85f30c7
Fudzuki <kiri11280201@gmail.com>
2022-12-07 13:01:50 +0900

    commit

--- routes/user.php
+++ routes/user.php
@@ -9,4 +9,4 @@ Route::get('/user/{id?}/work', [UserIndex::class, 'work'])->name('work');
 
 Route::get('/user/{id?}/work/post', [UserIndex::class, 'work_post'])->name('work_post');
 
-Route::post('/user/work-post', [UserIndex::class, 'work_post_']);
+Route::post('/user/{id?}/work/post', [UserIndex::class, 'work_post_']);

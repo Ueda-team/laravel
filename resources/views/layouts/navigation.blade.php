00818f471896e169fd3045470e46139dbdd42c39
Fudzuki <kiri11280201@gmail.com>
2022-12-08 16:32:22 +0900

    nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,36 +1,54 @@
-<header class="header">
-    <!--left-->
-    <div class="logo">
-        <a href="/">
-            <img src="{{ asset('img/logo.svg') }}" alt="ALTE">
-        </a>
+<div class="wrapper width: 100%;">
+    <!--header-->
+    <header class="header width: 100%;
+    display: flex;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.05);">
+        <!--left-->
+        <div class="logo width: 40%;
+    display: flex;
+    margin: auto;
+    padding: auto;">
+            <a href="index.php">
+                <img class=" width: 130px;
+    margin: 0 10px;" src="img/logo.png" alt="ALTE">
+            </a>
 
 
-        <!--search-->
-        <div class="search">
-            <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+            <!--search-->
+            <div class="search">
+                <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-            <form method="get" action="#" class="search_container">
-                <label>
-                    <input type="text" size="25" placeholder="キーワード検索">
-                </label>
-                <input type="submit" value="&#xf002">
-            </form>
+                <form method="get" action="#" class="search_container">
+                    <input type="text" size="30" placeholder="キーワード検索">
+                    <input type="submit" value="&#xf002">
+                </form>
+            </div>
+        </div><!--/logo-->
+        <!--/search-->
+        <!--/left-->
+        <!--nav-->
+        <nav class="navigation">
+            <ul>
+                <li><a href=""><img src="img/cart.png" alt="cart"></a></li>
+                <li><a href=""><img src="img/auction.png" alt="info"></a></li>
+                <li><a href=""><img src="img/heart.png" alt="auction"></a></li>
+                <li><a href=""><img src="img/bell.png" alt="iine"></a></li>
+                <li><a href=""><img src="img/human.png" alt="mypage"></a></li>
+            </ul>
+        </nav>
+        <!--/nav-->
+    </header>
+    <!--header-->
+    <!--header2-->
+    <header class="header2">
+        <!--left-->
+        <div class="category-left">
         </div>
-    </div><!--/logo-->
-    <!--/search-->
-    <!--/left-->
-
-    <!--nav-->
-    <nav class="navigation">
-        <ul>
-            <li><a href=""><img src="./icon/カート.png" alt="cart"></a></li>
-            <li><a href=""><img src="./icon/オークション.png" alt="info"></a></li>
-            <li><a href=""><img src="./icon/ハート.png" alt="auction"></a></li>
-            <li><a href=""><img src="./icon/ベル.png" alt="iine"></a></li>
-            <li><a href="{{ url('/dashboard') }}"><img src="./icon/人物.png" alt="mypage"></a></li>
-        </ul>
-    </nav>
-    <!--/nav-->
-</header>
-<!--header-->
+        <!--/left-->
+        <!--right-->
+        <div class="category-right">
+        </div>
+        <!--/right-->
+    </header>
+    <!--/header2-->
+</div><!--/wrapper-->

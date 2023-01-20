684a2cc41e85c6578ce8b4e62d8bca298b34b301
Fudzuki <kiri11280201@gmail.com>
2023-01-20 12:17:14 +0900

    オークションの追加1

---
+++ database/migrations/2023_01_18_074253_create_auctions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auctions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('start_price');
+            $table->integer('max_price');
+            $table->timestampTz('start_date');
+            $table->timestampTz('end_date');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auctions');
+    }
+};
--- resources/views/user/work.blade.php
+++ resources/views/user/work.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="work_wrap">
-        <a class="work_link" href="{{ url('/user/' .  Auth::user()->user_id . '/work/post') }}">出品する</a>
+        <a class="work_link" href="{{ url('/user/work/add') }}">マーケットに出す</a>
+        <a class="work_link" href="{{ url('/user/auction/add') }}">オークションに出す</a>
         <p class="work_p">出品中の作品</p>
 
         <?php

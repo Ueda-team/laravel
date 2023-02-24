7ee6648588e6a8d952f41bc4b6db3b56199b8d2c
ueda2001 <sara_melody_0927@yahoo.co.jp>
2023-02-24 15:27:28 +0900

    ページネーションcss

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -23,7 +23,7 @@
         </div>
 
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-            <div class="">
+            <div class="page_css">
                 <span class="relative z-0 inline-flex shadow-sm rounded-md">
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
